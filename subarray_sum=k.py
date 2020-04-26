a76fecd7d413a71933f653b47f20ae01f1081d93
IMSRIJAN04 <srijanerroju@gmail.com>
2020-04-26 17:03:03 +0530

    subarray_sum=k finished

--- subarray_sum=k.py
+++ subarray_sum=k.py
@@ -1,10 +1,36 @@
-arr=[1,2,3,4,5]
-n=len(arr)
-size=1
-for k in range(n):
-    for i in range(k,n):
-        print("@@@@@")
-        for j in range(k,i+1):
-            print(arr[j])
-        
+#input
+nums=[3,4,7,2,-3,1,4,2]
+k=7
+#program
+
+n=len(nums)
+# General method of calculating the continous sum and storing in prefixsum List
+# psum=[]
+# sum=0
+# psum.append(sum)
+# for a in arr:
+#     sum+=a
+#     psum.append(sum)
+# print(psum)
+
+#now in the below approach we are not using prefixsum arr because we are using the dynamically generated "continous sum" values
+s=0
+count=0
+sdict={0:1} #creating the sum dict and since the initial sum value is 0 and it is occuring for the first time we are adding 0 with value 1
+
+#in this approach we are iterating the array only once and counting the no of subarrays with sum=k
+
+for num in nums:
+    s+=num
+    #checking if the sub array sum equals k or not and incrementing the count using sdict value
+    if s-k in sdict:
+        count+=sdict[s-k]
+    #updating sdict
+    if s in sdict:
+        sdict[s]+=1
+    else:
+        sdict[s]=1
+
+print(count)
+
 
