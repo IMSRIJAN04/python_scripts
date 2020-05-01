8a7d346d6a2b5cfd5ee43b5b3915f167643aaf22
IMSRIJAN04 <srijanerroju@gmail.com>
2020-05-01 23:50:23 +0530

    1st unique_nw

---
+++ first_unique_num.py
@@ -0,0 +1,35 @@
+class FirstUnique:
+    def __init__(self, nums: List[int]):
+        self.a=nums
+        self.m={}
+        self.q=[]
+        for i in range(len(self.a)):
+            x=self.a[i]
+            if x in self.m:
+                self.m[x]=1+self.m[x]
+            else:
+                self.m[x]=1
+            self.q.append(self.a[i])          
+            
+    def showFirstUnique(self) -> int:
+        while True:
+            x=self.q[0]
+            z=self.m[x]
+            if z==1:
+                return self.m[x]
+            else:
+                if x>=0 and len(self.q)!=0:
+                    self.q.remove(x)
+                else:
+                    return -1
+            
+
+    def add(self, value: int) -> None:
+        self.m[value]+=1
+        self.q.append(value)
+
+
+# Your FirstUnique object will be instantiated and called as such:
+# obj = FirstUnique(nums)
+# param_1 = obj.showFirstUnique()
+# obj.add(value)
